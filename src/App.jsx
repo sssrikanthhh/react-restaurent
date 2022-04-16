539b82b752b639a0ba098ecf7008095e087092c0
srikanth <chiramanasrikanth265@gmail.com>
2022-04-16 18:51:56 +0530

    Add: Create Adding restaurents functionality
    - Create a form which accepts user input to create new restaurent
      and it adds to the restaurents list
    
    - Dispaly all the restaurents from restarents list using map

---
+++ src/App.jsx
@@ -0,0 +1,155 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import RestaurantDetails from './components/RestaurantDetails';
+import './App.css';
+
+function App() {
+	const [name, setName] = useState('');
+	const [image, setImage] = useState('');
+	// const [card, setCard] = useState(false);
+	// const [cash, setCash] = useState(false);
+	// const [upi, setUpi] = useState(false);
+	const [paymentMethods, setPaymentMethods] = useState({
+		card: false,
+		cash: false,
+		upi: false,
+	});
+	const [votes, setVotes] = useState(0);
+	const [review, setReview] = useState('');
+	const [categories, setCategories] = useState('');
+	const [itemCost, setItemCost] = useState(0);
+	const [restaurents, setRestaurents] = useState([]);
+
+	const createRestaurent = async e => {
+		e.preventDefault();
+		const singleRest = {
+			name,
+			image,
+			paymentMethods,
+			votes,
+			review,
+			categories: categories.split(','),
+			itemCost,
+		};
+		await axios.post('http://localhost:8080/restaurents', singleRest);
+	};
+
+	const fetchRestaurents = async () => {
+		const { data } = await axios('http://localhost:8080/restaurents');
+		setRestaurents(data);
+	};
+
+	useEffect(() => {
+		fetchRestaurents();
+	}, [restaurents]);
+	return (
+		<div className='app'>
+			<h1>React Restaurents</h1>
+
+			<div className='addRes'>
+				<h3>Add a new restaurent</h3>
+				<form onSubmit={createRestaurent}>
+					<div>
+						<label htmlFor='name'>Restaurent name: </label>
+						<input
+							type='text'
+							placeholder='Enter restaurent name'
+							id='name'
+							onChange={e => setName(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='image'>Image url:</label>
+						<input
+							type='text'
+							placeholder='Submit image url'
+							id='image'
+							onChange={e => setImage(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='votes'>Votes:</label>
+						<input
+							type='Number'
+							placeholder='Votes'
+							id='votes'
+							onChange={e => setVotes(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='review'>Review:</label>
+						<input
+							type='text'
+							placeholder='review'
+							id='review'
+							onChange={e => setReview(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='cost'>Cost per Item:</label>
+						<input
+							type='Number'
+							placeholder='item per cost'
+							id='cost'
+							onChange={e => setItemCost(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='categories'>Categories(comma values):</label>
+						<input
+							type='text'
+							placeholder='comma separated categories'
+							id='categories'
+							onChange={e => setCategories(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<h5>Payment methods:</h5>
+						<label htmlFor='card'>card</label>
+						<input
+							type='checkbox'
+							id='card'
+							checked={paymentMethods.card}
+							onChange={e =>
+								setPaymentMethods({ ...paymentMethods, card: e.target.checked })
+							}
+						/>
+
+						<label htmlFor='cash'>cash</label>
+						<input
+							type='checkbox'
+							id='cash'
+							checked={paymentMethods.cash}
+							onChange={e =>
+								setPaymentMethods({ ...paymentMethods, cash: e.target.checked })
+							}
+						/>
+
+						<label htmlFor='upi'>upi</label>
+						<input
+							type='checkbox'
+							id='upi'
+							checked={paymentMethods.upi}
+							onChange={e =>
+								setPaymentMethods({ ...paymentMethods, upi: e.target.checked })
+							}
+						/>
+					</div>
+
+					<button>Create a new restaurent</button>
+				</form>
+			</div>
+			{restaurents.map(restaurent => (
+				<RestaurantDetails key={restaurent.id} restaurent={restaurent} />
+			))}
+		</div>
+	);
+}
+
+export default App;
